a3b5fde177741f5b4b74d007f5943766a2718e0f
Benjámin Zvolenszki <billostudio1@gmail.com>
2020-08-25 22:02:27 +0200

    Low productivity :/, except today - Middle page almost done - Most animations changed to jQuery .animate() and .fadeIn() animations I hope I will be done before the start of school.

--- site.js
+++ site.js
@@ -1,20 +1,27 @@
 $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top + "px"
-        }, 1000);
-      } // End if
-    });
+
+  $("#top-centerbox-image").delay(300).animate({marginBottom: 0}, 1500);
+  $("#animated-namedesc-box").delay(300).animate({marginBottom: 0}, 1500);
+  $(".animated-desc-box").delay(1800).fadeIn(6000);  
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top + "px"
+      }, 1000);
+    } // End if
   });
+
+});
+
